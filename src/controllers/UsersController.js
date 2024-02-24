ced1a390a719347dee0a8c499fef96d07dcf8f34
leandro.citrodonegit config --global user.name leandro.citro <leandro_citro@hotmail.com>
2024-02-24 19:30:04 -0300

    gerando bash password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,39 +1,45 @@
-const { hash, compare} = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 const AppError = require("../utils/AppError");
 
 const express = require("express");
 
 const knex = require("../database/knex");
+const { EMPTY } = require("sqlite3");
 
 
 class UsersController {
+
     async create(request, response) {
 
         //console.log(request.body);
-        
-       const { name, email, password, isAdmin } = request.body;
 
-       const checkUsersExists = await knex("users").select('email').where({email});
-       
+        const { name, email, password, isAdmin } = request.body;
 
-       if(checkUsersExists){
+        const checkUsersExists = await knex("users").select('email').where({ email });
+
+        //console.log(checkUsersExists.length);
+
+       if (checkUsersExists.length > 0) {
             throw new AppError("Este e-mail já está cadastrado.");
-       }
 
-       
+        }
 
-        
-       await knex("users").insert({
+        const hashedPassord = await hash(password, 8);
+
+        await knex("users").insert({
             name,
             email,
-            password,
+            password: hashedPassord,
             isAdmin
-        });
-        
-       return response.status(201).json(`User created sucessfully!`);
-       //return response.status(201).json({checkUsersExists});
-    } 
+        }) 
+
+
+        return response.status(201).json(`User created sucessfully!`);
+
+
+
+    }
 
 }
 
